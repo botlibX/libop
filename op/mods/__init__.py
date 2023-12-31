a17efe5879faca258d1e3eed144a8752a2d9f11c
Bart Thate <bthate@dds.nl>
2023-12-31 20:36:49 +0100

    fix tinder

--- op/mods/__init__.py
+++ op/mods/__init__.py
@@ -17,7 +17,6 @@ from . import cmd, dbg, err, fnd, irc, log, mbx, mdl, mod, mre, pwd, req, rss, r
 def __dir__():
     return (
         'cmd',
-        'dbg',
         'err',
         'fnd',
         'irc',
